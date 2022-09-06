ee90f1f5933fd5256ab8dd768ee24c4bf2bdb26e
huaple <150332096+huaple@users.noreply.github.com>
2022-09-06 18:26:39 +0000

    Integration Orange3-Shadow

--- KNN Classifier/knn.py
+++ KNN Classifier/knn.py
@@ -32,4 +32,5 @@ def getdata(filename):
 def cosine_similarity(v1, v2):
 
     sum_xx, sum_xy, sum_yy = 0.0, 0.0, 0.0
- #   print "len: %d" %(len(v1)
+ #   print "len: %d" %(len(v1))
+    for i in range(0, len(v1)-1):
