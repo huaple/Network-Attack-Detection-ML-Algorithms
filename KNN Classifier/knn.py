7c04f8d6d6ab81e4167739d1d58b1f45a4430be0
huaple <150332096+huaple@users.noreply.github.com>
2023-01-19 05:51:52 +0000

    KEYS: Use the compat keyctl() syscall wrapper on Sparc64 for Sparc32 compat

--- KNN Classifier/knn.py
+++ KNN Classifier/knn.py
@@ -110,4 +110,6 @@ def knn_predict(test_data, train_data, k_value, category):
 
  #       max_index = recover_key(classNum, max_value)
  #       print "max_index %s" %(max_index)
-        i.append(max_value) # append predict
+        i.append(max_value) # append prediction; tag category
+
+def recover_key(dic
