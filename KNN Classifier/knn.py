f6367c46ba8634e208aad5fd25a94c593ff9aae6
huaple <150332096+huaple@users.noreply.github.com>
2023-02-25 13:19:54 +0000

    small comment fix

--- KNN Classifier/knn.py
+++ KNN Classifier/knn.py
@@ -135,4 +135,5 @@ def accuracy(test_data, true_result):
 
 def KNN(K, train_x, train_y, test_x, test_y):
    # dataset = getdata('UNSW_NB15_training-set_selected.csv')  # getdata function call with csv file as parameter
-#    p
+#    print len(dataset)
+ #   train_dataset,
