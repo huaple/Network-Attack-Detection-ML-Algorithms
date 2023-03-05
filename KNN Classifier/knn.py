692fb9b61421390ae5881af91c74d96074340854
huaple <150332096+huaple@users.noreply.github.com>
2023-03-05 20:11:19 +0000

    usage instructions

--- KNN Classifier/knn.py
+++ KNN Classifier/knn.py
@@ -141,4 +141,4 @@ def KNN(K, train_x, train_y, test_x, test_y):
 
     train_dataset = train_x.tolist()
     print "Number of training X: %s" %len(train_dataset)
-    pr
+    print "Number of training Y:
