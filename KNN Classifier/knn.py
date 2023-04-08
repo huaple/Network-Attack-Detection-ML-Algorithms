a665a7008bdc7ba068d55c20d2f5f9359d561d39
huaple <150332096+huaple@users.noreply.github.com>
2023-04-08 17:35:31 +0000

    doc string

--- KNN Classifier/knn.py
+++ KNN Classifier/knn.py
@@ -151,4 +151,4 @@ def KNN(K, train_x, train_y, test_x, test_y):
     knn_predict(test_dataset, train_dataset, K, train_y)
     atrain = round(accuracy(train_dataset, train_y),5)
     TrainError = float(100.00000- float(atrain))
-    at
+    atest = round(accuracy(test_d
